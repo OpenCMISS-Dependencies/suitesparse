68946139ad16c15df1de4e50604960e69bc5d511
Daniel Wirtz <daniel.wirtz@simtech.uni-stuttgart.de>
2014-10-23 11:03:12 +1300

    removed metis version (now is as direct dependency as mumps also only links against metis4) amd tests now working

---
+++ AMD/Demo/testhelper.cmake
@@ -0,0 +1,13 @@
+execute_process(COMMAND ${CMD} 
+    OUTPUT_VARIABLE CMD_RESULT 
+    RESULT_VARIABLE CMD_RETCODE)
+file(WRITE tmp "${CMD_RESULT}")
+execute_process(COMMAND ${CMAKE_COMMAND} 
+    -E compare_files tmp ${TARGET}
+    RESULT_VARIABLE CMP_RETCODE)
+if(CMD_RETCODE OR CMP_RETCODE)
+    message(STATUS "diff: --------------------")       
+    execute_process(COMMAND diff tmp ${TARGET})
+    message(FATAL_ERROR "Test binary ${CMD} failed: CMD_RETCODE=${CMD_RETCODE}, CMP_RETCODE=${CMP_RETCODE}")
+endif()
+file(REMOVE tmp)
