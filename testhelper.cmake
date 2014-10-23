f1b856234e2f6e4997e2cd0fcfdf1c15ae42f083
Daniel Wirtz <daniel.wirtz@simtech.uni-stuttgart.de>
2014-10-23 14:10:26 +1300

    now working with tests included for UMFPACK umfpack tests failing only due to test runtime included in test case (bad testing)

--- testhelper.cmake
+++ testhelper.cmake
@@ -6,8 +6,7 @@ execute_process(COMMAND ${CMAKE_COMMAND}
     -E compare_files tmp ${TARGET}
     RESULT_VARIABLE CMP_RETCODE)
 if(CMD_RETCODE OR CMP_RETCODE)
-    message(STATUS "diff: --------------------")       
-    execute_process(COMMAND diff tmp ${TARGET})
+    file(WRITE ${TARGET}.testout "${CMD_RESULT}")       
     message(FATAL_ERROR "Test binary ${CMD} failed: CMD_RETCODE=${CMD_RETCODE}, CMP_RETCODE=${CMP_RETCODE}")
 endif()
 file(REMOVE tmp)
